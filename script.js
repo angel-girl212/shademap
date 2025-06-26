77ccc2aae1c47753f5c864dcd3f5ea1dce8ff844
Kyra Meier <kyraprmeier@gmail.com>
2025-06-26 11:29:06 -0400

    updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-var map = L.map('map').setView({x}/{y}/{z});
+var map = L.map('map').setView([43.63, -79.40], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/13/4363/-7940.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
