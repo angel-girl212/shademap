084ba07b18f1c972d2d122425a2bdb9090852604
Kyra Meier <kyraprmeier@gmail.com>
2025-06-26 11:10:24 -0400

    trying bway coords

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var map = L.map('map').setView([44.09, 27.41], 13);
+var map = L.map('map').setView([43.63, -79.40], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/13/4409/2741.png', {
+L.tileLayer('https://tile.openstreetmap.org/13/4363/-7940.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
