19ca53c85f3814230b09a1a2e7a6d1d064536de1
Kyra Meier <kyraprmeier@gmail.com>
2025-06-26 09:41:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([-79.40, 43.63], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://tile.openstreetmap.org/13/-79.40/46.63.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
