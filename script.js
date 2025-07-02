66e7dc9429a05f13e0e16e41d9c63ae9979fc8dd
Kyra Meier <kyraprmeier@gmail.com>
2025-07-02 09:43:12 -0400

    hmmmmmmmmmmm

--- script.js
+++ script.js
@@ -99,11 +99,8 @@ function sendToForm(e) {
 }
 
 function createPinpoint(e) {
-  var coord = e.latlng.toString().split(',');
-  var lat = coord[0].split('(');
-  var lng = coord[1].split(')');
-  var user_marker = L.marker([lat, lng]).addTo(map);
-  user_marker.bindPopup("<b>User Generated Shade Spot").openPopup();
+  L.marker(e.latlng).addTo(map);
+  marker.bindPopup("<b>User Generated Marker</b><br>hello world").openPopup();
 }
 
 map.addEventListener("dblclick", (e) => {
