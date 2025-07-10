025aeace851fffd5317c94b82182152006b3e6ef
Kyra Meier <kyraprmeier@gmail.com>
2025-07-10 13:12:41 -0400

    adding multiple choice for time

--- script.js
+++ script.js
@@ -39,26 +39,40 @@ marker.bindPopup("<b>The Bentway</b><br>250 Fort York").openPopup();
 // polygon.bindPopup("<b>Toronto Regional Boundary</b><br>source: open data portal");
 
 function add(e) {
-  // var coord = e.latlng.toString().split(',');
-  // var lat = coord[0].split('(');
-  // var lng = coord[1].split(')');
-  //alert("You added a shady spot at" + lat[1] + " and " + lng[0]);
-  // const markerName = prompt(`You added a shady spot at ${lat[1]} and ${lng[0]}. What would like to name it?`);
   const markerName = prompt(`You clicked a spot at ${e.latlng.lat.toFixed(5)} and ${e.latlng.lng.toFixed(5)}. Add a name to submit`);
   
   if (markerName) {
-    const description = prompt(`Describe your shady spot`); // lmfao
+    const description = prompt(`Describe your shady spot`);
+    
+    const choice = prompt("What time do you benefit fromthis shady space:\n1. Morning\n2. Midday\n3. Evening \n4. Night", "");
+
+    let timeday = ""
+    switch (choice) {
+      case "1":
+        timeday = "Morning";
+        break;
+      case "2":
+        timeday = "Midday";
+        break;
+      case "3":
+        timeday = "Evening";
+        break;
+      case "4":
+        timeday = "Night";
+        break;
+      default:
+        alert("invalid input") return;
+}
+    
     const marker = L.marker(e.latlng).addTo(map);
     marker.bindPopup(`<b>${markerName}</b><br>${e.latlng.toString()}`).openPopup();
-    sendToForm(e, markerName, description);
+    sendToForm(e, markerName, description, timeday);
  }
 }
 
-function sendToForm(e, markerName, description) {
+function sendToForm(e, markerName, description, timeday) {
   const lat = e.latlng.lat.toFixed(5);
   const lng = e.latlng.lng.toFixed(5);
-  // const timeStamp = new Date().toISOString();
-  // const userId = "user-" + Math.floor(Math.random() * 100000);
 
   const formUrl = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfNV5ldiWUsR3nYRD35-_m2W4TSuUuijP3L55uOLdtPwqC2AQ/formResponse";
   const formData = new URLSearchParams();
@@ -66,8 +80,7 @@ function sendToForm(e, markerName, description) {
   formData.append("entry.1956546171", lng);
   formData.append("entry.1519028228", markerName);
   formData.append("entry.772410688", description);
-  // formData.append("entry.56758637", timeStamp);  // timestamp optional field
-  // formData.append("entry.1570862743", userId); // userId
+  formData.append("entry.242720066", timeday);
 
   fetch(formUrl, {
     method: "POST",
@@ -76,4 +89,4 @@ function sendToForm(e, markerName, description) {
   }).catch(err => console.error("Error:", err));
 }
 
-map.addEventListener('click', add); // not gonna pretend i undrstand that
+map.addEventListener('click', add);
