d4f78cbc885daef17eb4415af9ac0a7debd024ab
Kyra Meier <kyraprmeier@gmail.com>
2025-07-18 16:11:20 -0400

    format

--- script.js
+++ script.js
@@ -192,7 +192,11 @@ map.on('click', add);
 const greyIcon = new L.Icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-grey.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-  iconSize: [25, 41], iconAnchor: [12, 41], popupAnchor: [1, -34], shadowSize: [41, 41]
+  iconSize: [25, 41], 
+  iconAnchor: [12, 41], 
+  popupAnchor: [1, -34], 
+  shadowSize: [41, 41]
+  // opacity?
 });
 
 // Load and parse already submitted markers CSV
